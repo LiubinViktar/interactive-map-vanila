1d48029fdc441a4f0fe2c620f62a78515152fd18
LiubinViktar <liubinviktar@gmail.com>
2022-04-12 15:32:38 +0300

    implemented fetch AJAX. edited readme/branch-info

---
+++ README.md
@@ -0,0 +1,13 @@
+Interactive svg-map. pure javascript:
+
+modules:
+1. branch-info - contains main info about regions from map
+2. draw-branch-body - creates block with branch's info
+3. edit-branch - rerender branch information via navigation
+4. interaction-with-map - event listeners and elements style interactions
+5. map - svg map
+
+npm install wabpack webpack-cli --save-dev
+npm install json-server 
+npx webpack
+npx json-server db.json
--- js/services/services.js
+++ js/services/services.js
@@ -1,18 +1,6 @@
 'use strict';
 /* jshint esversion: 8 */
 
-const postData = async (url, data) => {
-    const result = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },                
-        body: data
-    });
-
-    return await result.json();
-};
-
 const getData = async (url) => {
     let result = await fetch(url);
 
@@ -24,4 +12,4 @@ const getData = async (url) => {
     return await result.json();
 };
 
-export {postData, getData};
+export default getData;
